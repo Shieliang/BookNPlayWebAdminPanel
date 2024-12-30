25723f319860802c1af2756959aee8432b747551
Shieliang <shieliang22@gmail.com>
2024-12-30 12:29:50 +0800

    function full done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,4 @@
-@app.route('/facility_management', methods=['GET'])
-def facility_management():
-    if 'admin' not in session:
-        return redirect(url_for('login'))
-
-    try:
-        facilities = list(facility_collection.find({}))
-        return render_template('facility_management.html', facilities=facilities)
-
-    except Exception as e:
-        print(f"Error fetching facilities: {str(e)}")
-        return render_template('facility_management.html', error="Error fetching facilities.", facilities=[])
+@app.route('/logout')
+def logout():
+    session.pop('admin', None)
+    return redirect(url_for('login'))
